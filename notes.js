5e8b9d10a50463e7357a4a0f4903f29809768b08
Salim <salim.hassan.welleat@gmail.com>
2022-10-05 20:46:59 +0200

    add: notes file

---
+++ notes.js
@@ -0,0 +1,10 @@
+
+///**** */
+//Create new user CREATE USER Bankroot
+// CREATE USER  bankroot WITH PASSWORD 'password' CREATEDB ;
+
+//CONNECT as a new user bankroot
+//psql -U bankroot -d postgres 
+//CREATE DATABASE bank-node
+// to list all the database \l, \c + databasename to move to another database
+// \d users 
